eebcb981e53c00659ce64ff0998b7fc1b8967e02
Michael Ashe <mickjohnashe@hotmail.com>
2017-02-21 20:42:48 +0000

    Changes to the style.css for responsive design.
    Lots of changes to the script.js. Removing functions and some tudying
    up. Biggest change is removing the 'generate_filter_buttons' function.
    The buttons are now generated inside the template.

--- static/script.js
+++ static/script.js
@@ -1,46 +1,66 @@
 
-function get_event_date_range(e) {
-  if (e.sessions.length === 0) {
-    return "TBD";
-  } else if (e.sessions.length === 1) {
-    return e.sessions[0].date;
-  } else {
-    var start = e.sessions[0].date;
-    var len = e.sessions.length;
-    var end = e.sessions[len-1].date;
-    return start + ", " + end;
-  }
-}
+var globals = {
+  right_triangle: '&#x25BA',
+  down_triangle: '&#x25BC'
+};
 
-function toggle_button_clicked(button, sport) {
+function toggle_button_clicked(button, sport_type) {
   var class_list = button.classList;
 
   // If it's on turn it off, and hide it's events
   if (in_array(class_list, 'filter-button-on')) {
     button.classList.add("filter-button-off");
     button.classList.remove("filter-button-on");
-    filter_table(sport, true);
+    filter_table(sport_type, true);
   } else {
     button.classList.remove("filter-button-off");
     button.classList.add("filter-button-on");
-    filter_table(sport, false);
+    filter_table(sport_type, false);
   }
 }
 
-function toggle_sessions(event_row, session_table_id) {
-  var session_row = document.getElementById(session_table_id);
-  var triangle = event_row.getElementsByTagName("td")[0];
-
-  var class_list = session_row.classList;
-  if (in_array(class_list, 'sessions-row')) {
-    session_row.classList.add('sessions-row-hidden');
-    session_row.classList.remove('sessions-row');
-    triangle.innerHTML = '&#x25BA';
+function toggle_side_nav() {
+  var side_nav = document.getElementById('mySidenav');
+  var nav_button = document.getElementById('nav-button');
+  if (side_nav.style.display == 'none' || side_nav.style.display == '') {
+    side_nav.style.display = 'block';
+    nav_button.style.marginLeft = '220px';
   } else {
-    session_row.classList.remove('sessions-row-hidden');
-    session_row.classList.add('sessions-row');
-    triangle.innerHTML = '&#x25BC';
+    nav_button.style.marginLeft = '0';
+    side_nav.style.display = 'none';
   }
+}
+
+function hide_session_row(session_row) {
+  if(!in_array(session_row.classList, 'session-row-hidden')) {
+    session_row.classList.add('sessions-row-hidden');
+  }
+  session_row.classList.remove('sessions-row');
+}
+
+function show_session_row(session_row) {
+  session_row.classList.remove('sessions-row-hidden');
+  if(!in_array(session_row.classList, 'session-row')) {
+    session_row.classList.add('sessions-row');
+  }
+}
+
+function toggle_session_row(event_row) {
+  var table = document.getElementById('events_table');
+  var session_row = table.rows[event_row.rowIndex + 1];
+
+  if (in_array(session_row.classList, 'sessions-row')) {
+    hide_session_row(session_row);
+    set_event_row_triangle(event_row, globals.right_triangle);
+  } else {
+    show_session_row(session_row);
+    set_event_row_triangle(event_row, globals.down_triangle);
+  }
+}
+
+function set_event_row_triangle(event_row, new_triangle) {
+  var triangle_element = event_row.getElementsByTagName("td")[0];
+  triangle_element.innerHTML = new_triangle;
 }
 
 function in_array(array, item) {
@@ -53,68 +73,53 @@ function in_array(array, item) {
 }
 
 function filter_table(query, hide) {
-  var table, tr, td;
-  table = document.getElementById("events_table");
-  tr = table.getElementsByTagName("tr");
+  var table = document.getElementById("events_table");
+  var table_rows = table.rows;
 
   // Loop through all table rows, and hide those who don't match the search query
-  for (var i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
+  for (var i = 0; i < table_rows.length; i++) {
+    var current_row = table_rows[i];
+    var td = current_row.getElementsByTagName("td")[1];
     if (td) {
       if (td.innerHTML.toUpperCase().indexOf(query.toUpperCase()) > -1) {
         if (hide) {
-          tr[i].style.display = "none";
+          /* If the sessions for this row are expanded, hide them */
+          /* First get the 'session_row'. This is the current row + 1 */
+          var session_row = table_rows[current_row.rowIndex + 1];
+          /* We also need set the triangle*/
+          set_event_row_triangle(current_row, globals.right_triangle);
+          /* Then hide the session row */
+          hide_session_row(session_row);
+          /* And then hide the row */
+          current_row.style.display = "none";
         } else {
-          tr[i].style.display = "";
+          current_row.style.display = "";
         }
       } 
     } 
   }
 }
 
-function get_list_of_sport_names() {
-  var table = document.getElementById("events_table");
-  var rows = table.getElementsByTagName("tr");
-  var sports = [];
+// Filter out duplicate items in an array
+// http://stackoverflow.com/questions/11688692/most-elegant-way-to-create-a-list-of-unique-items-in-javascript
 
-  for (var i = 0; i < rows.length; i++) {
-    var class_list = rows[i].classList;
-    // Only want to check events_table_row.
-    if (in_array(class_list, 'events_table_row')) {
-      var td = rows[i].getElementsByTagName("td")[1];
-      if (td) {
-        sports.push(td.innerHTML.trim());
-      }
-    }
+// http://stackoverflow.com/questions/11309859/css-media-queries-and-javascript-window-width-do-not-match
+function viewport() {
+  var e = window, a = 'inner';
+  if (!('innerWidth' in window )) {
+    a = 'client';
+    e = document.documentElement || document.body;
   }
-  return sports;
+  return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
 }
 
-//http://stackoverflow.com/questions/11688692/most-elegant-way-to-create-a-list-of-unique-items-in-javascript
-function unique(arr) {
-  var u = {}, a = [];
-  for(var i = 0, l = arr.length; i < l; ++i){
-    if(!u.hasOwnProperty(arr[i])) {
-      a.push(arr[i]);
-      u[arr[i]] = 1;
-    }
+window.onresize = function(event) {
+  var v = viewport();
+  if (v.width >= 1023) {
+    var side_nav = document.getElementById('mySidenav');
+    side_nav.style.display = 'block';
+  } else {
+    var side_nav = document.getElementById('mySidenav');
+    side_nav.style.display = 'none';
   }
-  return a;
-}
-
-// Generate the filter buttons based on the sport names
-function generate_filter_buttons() {
-  var sports = unique(get_list_of_sport_names());
-  var sidenav_div = document.getElementById("mySidenav");
-
-  for(var i = 0; i < sports.length; i++) {
-    var sport_name = sports[i];
-    var button = document.createElement("A");
-    var button_text = document.createTextNode(sport_name);
-    button.appendChild(button_text);
-    button.setAttribute("class", "filter-button filter-button-on");
-    button.setAttribute("href", "#");
-    button.setAttribute("onclick", "toggle_button_clicked(this,'" + sport_name + "')");
-    sidenav_div.appendChild(button);
-  }
-}
+};
